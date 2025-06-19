4dc3113baf21f08ece26cbb33c563394f31755e7
ix1osbek <bekerkinov2004@gmail.com>
2025-06-19 18:53:53 +0500

    Login

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { AuthService } from './users.service';
+import { AuthController } from './users.controller';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService],
+  controllers: [AuthController],
+  providers: [AuthService],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from '../users/user.model';
+import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+export class AuthService {
+  constructor(
+    @InjectModel(User)
+    private userModel: typeof User,
+    private configService: ConfigService,
+  ) {}
+
+  async validateUser(phone_number: string, password: string): Promise<any> {
+    const user = await this.userModel.findOne({ where: { phone_number } });
+    if (user && await bcrypt.compare(password, user.password)) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
   }
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async login(phone_number: string, password: string): Promise<{ access_token: string }> {
+    const user = await this.validateUser(phone_number, password);
+    if (!user) {
+      throw new Error('Invalid credentials');
+    }
+    const payload = { user_id: user.user_id, role: user.role };
+    return {
+      access_token: jwt.sign(payload, String(process.env.JWT_SECRET), { expiresIn: '1h' }),
+    };
   }
 }
